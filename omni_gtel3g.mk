88a5beab9b19b9c574536a3f8b325514280264e5
KrutosVIP <52695559+KrutosVIP@users.noreply.github.com>
2023-07-13 15:15:21 +0000

    gtel3g: TWRP-ify

--- omni_gtel3g.mk
+++ omni_gtel3g.mk
@@ -23,7 +23,6 @@
 #$(call inherit-product, device/samsung/v1awifi/device.mk)
 # This is where we'd set a backup provider if we had one
 #$(call inherit-product, device/sample/products/backup_overlay.mk)
-$(call inherit-product, vendor/pb/config/common.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
